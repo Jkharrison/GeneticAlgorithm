89994f4364285f6257afdc971f2ef7c00e09567e
Jkharrison <32682899+Jkharrison@users.noreply.github.com>
2018-09-18 17:42:16 -0500

    Population won a few times but taking some time to win.

--- Game.java
+++ Game.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.TreeMap;
+import java.util.Comparator;
+
+
 
 class Game
 {
@@ -7,10 +11,12 @@ class Game
 	static double[] evolveWeights() throws Exception
 	{
 		// Meta-Parameters.
+		TreeMap<Integer, Integer> fitness = new TreeMap<>();
 		float mutationRate;
 		float averageDevation = .06f;
-		int numOfBattles = 15;
+		int numOfBattles = 50;
 		int numOfMates = 0;
+		int winningBonus = 100;
 		// Create a random initial population
 		boolean neuralWins = false; // Check if evolved weights beat the reflex agent.
 		Random r = new Random();
@@ -25,17 +31,18 @@ class Game
 		// todo: YOUR CODE WILL START HERE.
 		//       Please write some code to evolve this population.
 		//       (For tournament selection, you will need to call Controller.doBattleNoGui(agent1, agent2).)
-		while(!neuralWins)
+		int iterations = 0;
+		while(!neuralWins || iterations < 1000)
 		{
 			// Promote Diversity
 			for(int i = 0; i < population.rows(); i++)
 			{
 				double[] chromosome = population.row(i);
-				mutationRate = r.nextInt(99);
+				mutationRate = r.nextInt(100);
 				if(mutationRate >= 60) // Probability that Chromosome should be mutated.
 				{
 					chromosome[r.nextInt(population.cols())] += r.nextGaussian() * averageDevation;
-				}	
+				}
 			}
 			// Natural Selection
 			for(int i = 0; i < numOfBattles; i++)
@@ -43,18 +50,18 @@ class Game
 				// Choose random number of battles to occur and choose the red and blue agents.
 				int blue = r.nextInt(population.rows());
 				int red = r.nextInt(population.rows());
-				int lives = r.nextInt(99);
+				int lives = r.nextInt(100);
 				if(Controller.doBattleNoGui(new NeuralAgent(population.row(blue)), new NeuralAgent(population.row(red))) == 1)
 				{
 					// Choosing if winner lives or dies.
-					if(lives >= 51)
+					if(lives >= 40)
 						population.removeRow(red);
 					else
 						population.removeRow(blue);
 				}
 				else
 				{
-					if(lives >= 51)
+					if(lives >= 40)
 						population.removeRow(blue);
 					else
 						population.removeRow(red);
@@ -87,39 +94,61 @@ class Game
 				}
 				double[] daddy = population.row(similarParent);
 				double[] child = population.newRow();//new double[parent.length];
-				for(int i = 0; i < parent.length; i++)
+				int crossOverPoint = r.nextInt(population.cols());
+				for(int i = 0; i < crossOverPoint; i++)
 				{
-					int choice = r.nextInt(2); // 0, 1
-					if(i == 0)
-					{
 						child[i] = parent[i];
-					}
-					else
-						child[i] = daddy[i];
+				}
+				for(int i = crossOverPoint; i < population.cols(); i++)
+				{
+					child[i] = daddy[i];
 				}
 			}
-			// for(int i = 0; i < population.rows(); i++)
-			// {
-			// 	System.out.println("Getting here");
-			// 	if(Controller.doBattleNoGui(new ReflexAgent(), new NeuralAgent(population.row(i))) == -1)
-			// 	{
-			// 		neuralWins = true;
-			// 		System.out.println("Evolution complete");
-			// 		// return population.row(i);
-			// 	}
-			// }
-			if(Controller.doBattleNoGui(new ReflexAgent(), new NeuralAgent(population.row(0))) == -1)
+			for(int i = 0; i < population.rows(); i++)
 			{
-				neuralWins = true;
-				System.out.println("Evolution complete");
-				// return population.row(i);
+				// Time stamp
+				long startTime = System.currentTimeMillis();
+				if(Controller.doBattleNoGui(new ReflexAgent(), new NeuralAgent(population.row(i))) == -1)
+				{
+					// Time stamp to win. bonus if winning.
+					long endTime = System.currentTimeMillis();
+					int difference = (int)(endTime - startTime);
+					fitness.put(i, difference + winningBonus); // 100 for winningBonus.
+					System.out.println("Time to win: " + difference);
+					neuralWins = true;
+					// Controller.doBattle(new ReflexAgent(), new NeuralAgent(population.row(i)));
+					System.out.println("Evolution complete");
+					return population.row(i);
+					// return population.row(i);
+				}
+				else
+				{
+					long endTime = System.currentTimeMillis();
+					int difference = (int)(endTime - startTime);
+					fitness.put(i, difference);
+					//System.out.println("Time to lose: " + difference);
+					// Time stamp to lose. Delta time ms lose.
+				}
 			}
-			// Controller.doBattleNoGui(new ReflexAgent(), new NeuralAgent(population));
+			int mostFit = Integer.MIN_VALUE;
+			int mostFitIndex = -1;
+			for(int i = 0; i < population.rows(); i++)
+			{
+				int fitnessScale = fitness.get(i);
+				if(fitnessScale > mostFit)
+				{
+					mostFit = fitnessScale;
+					mostFitIndex = i;
+				}
+			}
+			System.out.println("Most fit population: " + mostFit + " at index: " + mostFitIndex);
+			// System.out.println(fitness);
+			iterations++;
 		}
 
 
 		// Return an arbitrary member from the population
-		return population.row(0);
+		return population.row(20);
 	}
 	static int similar(double[] parent, double[] selectedParent)
 	{
